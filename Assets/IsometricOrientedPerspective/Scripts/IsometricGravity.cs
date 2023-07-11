360d66c9219f2029175d5d58654c5cd8d045d8a8
SilvaAniceto <silva.aniceto1418@gmail.com>
2023-07-10 23:47:58 -0300

    nada

--- Assets/IsometricOrientedPerspective/Scripts/IsometricGravity.cs
+++ Assets/IsometricOrientedPerspective/Scripts/IsometricGravity.cs
@@ -1,8 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
 namespace CharactersCreator
 {
-    public class IsometricGravity
+    public class IsometricGravity : MonoBehaviour
     {
         private static Vector3 m_gravityVector = Vector3.zero;
         private static Vector3 m_position = Vector3.zero;
@@ -14,7 +15,6 @@ namespace CharactersCreator
         public Vector3 Vector { get { return m_gravityVector; } set {if (value == m_gravityVector) return; m_gravityVector = value; } }
         public Vector3 Position { get { return m_position; } set { m_position = value; } }
         public CapsuleCollider Collider { get { return m_capsuleCollider; } set { m_capsuleCollider = value; } }
-        public float GroundRadiusCheck { get { return m_groundRadiusCheck;} }
         public LayerMask LayerMask { get { return m_layerMask; } set { m_layerMask = value; } }
 
         public static Vector3 GravityForce()
@@ -27,20 +27,27 @@ namespace CharactersCreator
 
             ground = Physics.CheckSphere(m_position - new Vector3(0, 0.5f, 0), m_capsuleCollider.radius / m_groundRadiusCheck, m_layerMask, QueryTriggerInteraction.Collide);
 
-            return ground;
-        }
-        public static void Jump(float jumpHeight, float jumpForce, bool jumpInput)
-        {
-            if (OnGround() && m_gravityVector.y < 0)
+            if (ground && m_gravityVector.y < 0)
             {
                 m_gravityVector = Vector3.zero;
                 m_isJumping = false;
             }
 
+            return ground;
+        }
+        public static void Jump(float jumpHeight, float jumpForce, bool jumpInput)
+        {
+            float verticalDisplacement = Mathf.Sqrt(jumpHeight * jumpForce * Physics.gravity.y);
+
             if (jumpInput && OnGround())
             {
                 m_isJumping = true;
-                m_gravityVector += new Vector3(0, Mathf.Sqrt(jumpHeight * jumpForce * Physics.gravity.y), 0);
+                m_gravityVector = new Vector3(0, Mathf.Sqrt(jumpHeight * jumpForce * Physics.gravity.y), 0);
+            }
+
+            if (m_position.y >= Mathf.Sqrt(verticalDisplacement))
+            {
+                m_isJumping = false;
             }
 
             if (!jumpInput && m_isJumping)
