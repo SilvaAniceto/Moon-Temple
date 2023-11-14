850ffc18ed9c8bdb50e422ec99109425696ad4da
SilvaAniceto <69652978+SilvaAniceto@users.noreply.github.com>
2023-11-13 23:06:09 -0300

    Map Editor

--- Assets/Caça ao Tesouro/Scripts/Editor/MapEditor.cs
+++ Assets/Caça ao Tesouro/Scripts/Editor/MapEditor.cs
@@ -2,27 +2,41 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 namespace CustomGameController
 {
     [CustomEditor(typeof(MapCreator))]
     public class MapEditor : Editor
     {
-        SerializedProperty objects;
-        SerializedProperty mapProps;
+        MapCreator mapCreator = new MapCreator();
+
+        SerializedProperty propType;
+
+        int selectedIndex;
 
         private void OnEnable()
         {
-            objects = serializedObject.FindProperty("objects");
-            mapProps = serializedObject.FindProperty("mapProps");
+            propType = serializedObject.FindProperty("propType");
+            mapCreator = (MapCreator)target;
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(objects);
-            EditorGUILayout.PropertyField(mapProps);
+            EditorGUILayout.PropertyField(propType);
+
+            GUILayout.Space(10);
+
+            string[] id = mapCreator.CurrentProps.id.Trim().Split(',');
+            selectedIndex = EditorGUILayout.Popup("Prefab", selectedIndex, id);
+
+            //EditorGUI.DrawPreviewTexture(new Rect(25, 60, 100, 100), mapCreator.texture);
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Create")) mapCreator.InstatiateProp(selectedIndex);
 
             serializedObject.ApplyModifiedProperties();
         }
--- Assets/Caça ao Tesouro/Scripts/MapCreator.cs
+++ Assets/Caça ao Tesouro/Scripts/MapCreator.cs
@@ -1,35 +1,96 @@
 using UnityEngine;
+using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
 
 namespace CustomGameController
 {
-    public enum MapProps
-    {
-        None,
-        Buildings,
-        Natures,
-        Props,
-        Roads,
-        Vehicles
-    }
+    
 
+    [ExecuteInEditMode]
     public class MapCreator : MonoBehaviour
     {
-        [SerializeField] public List<GameObject> objects = new List<GameObject>();
-        [SerializeField] private string PropsFolder = "Assets/SimplePoly City - Low Poly Assets/Prefab";
-
-        [SerializeField] private MapProps mapProps = MapProps.None;
-        void Start()
+        [System.Serializable] public class Props
         {
-        
+            public List<GameObject> propsPrefab = new List<GameObject>();
+            public string id = "";
+        }
+        public enum PropsType
+        {
+            None,
+            Buildings,
+            Natures,
+            Props,
+            Roads,
+            Vehicles
         }
 
-        void Update()
+        PropsType _type;
+        PropsType Type 
         {
-            Debug.Log(mapProps);
-            Debug.Log(objects.Count);
+            get
+            {
+                return propType;
+            }
+            set
+            {
+                if (_type == value) return;
+
+                _type = value;
+
+                GetPropsFromFolder(_type.ToString());
+            }
+        }
+
+        [SerializeField] private PropsType propType = PropsType.None;
+        
+        private string PrefabResourceFolder = "Prefab/";
+        private string ThumbResourceFolder = "Textures/Custom/";
+
+        public Texture2D texture;
+        public Props CurrentProps = new Props();
+
+        private void Update()
+        {
+            Type = propType;
+        }
+
+        void GetPropsFromFolder(string propFolder)
+        {
+            CurrentProps.propsPrefab.Clear();
+            CurrentProps.id = "";
+
+            GameObject[] prefabs = Resources.LoadAll<GameObject>(PrefabResourceFolder + propFolder);
+            
+            foreach (GameObject prefab in prefabs)
+            {
+                if (!CurrentProps.propsPrefab.Contains(prefab))
+                {
+                    CurrentProps.propsPrefab.Add(prefab);
+                    CurrentProps.id += prefab.name + ",";
+                }
+            }
+        }
+
+        public void InstatiateProp(int index)
+        {
+            if (CurrentProps.propsPrefab == null) return;
+
+            PrefabUtility.InstantiatePrefab(CurrentProps.propsPrefab[index]);
+        }
+
+        void SaveCameraView(Camera cam)
+        {
+            RenderTexture screenTexture = new RenderTexture(Screen.width, Screen.height, 16);
+            cam.targetTexture = screenTexture;
+            RenderTexture.active = screenTexture;
+            cam.Render();
+            Texture2D renderedTexture = new Texture2D(Screen.width, Screen.height);
+            renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+            RenderTexture.active = null;
+            byte[] byteArray = renderedTexture.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/cameracapture.png", byteArray);
         }
     }
 }
